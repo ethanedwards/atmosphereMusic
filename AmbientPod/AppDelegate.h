7671f6e79373cc525d2925af2d308ae277c75473
ethanedwards <ethanmedwards@gmail.com>
2015-12-12 14:56:34 +0900

    Initial Commit

---
+++ AmbientPod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AmbientPod
+//
+//  Created by ethan on 12/12/15.
+//  Copyright (c) 2015 Ethan Edwards. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
